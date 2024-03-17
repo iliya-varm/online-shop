ef3511628d53b9b832536156acd849c1ee3a1a7b
iliya-varm <iliyavarnaserim@gmail.com>
2024-03-17 19:14:22 +0330

    products page for admin panen added

--- models/product.py
+++ models/product.py
@@ -4,8 +4,9 @@ from extentions import db
 
 class Product(db.Model):
 
-    __tablename__ = "products"
+    __tablename__ = 'products'
     id = Column(Integer, primary_key=True)
-    nane = Column(String, unique=True, nullable=False, index=True)
+    name = Column(String, unique=True, nullable=False, index=True)
     description = Column(String, nullable=False, index=True)
     price = Column(Integer, nullable=False, index=True)
+    active = Column(Integer, nullable=False, index=True)

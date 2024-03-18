d4e49565916047931fc269c213f970609c19b7ae
iliya-varm <iliyavarnaserim@gmail.com>
2024-03-18 16:14:55 +0330

    login and register and user added

--- blueprints/user.py
+++ blueprints/user.py
@@ -1,8 +1,7 @@
-from flask import Blueprint, render_template, request, redirect
+from flask import Blueprint, render_template, request, redirect, url_for, flash, abort
 from flask_login import login_user
 from passlib.hash import sha256_crypt
 from extentions import db
-import models.user
 from models.user import User
 
 app = Blueprint('user', __name__)
@@ -14,15 +13,35 @@ def login():  # put application's code here
         return render_template('user/login.html')
     else:
         register = request.form.get('register', None)
-        username = request.form.get('username')
-        password = request.form.get('password')
-        phone = request.form.get('phone')
-        address = request.form.get('address')
+        username = request.form.get('username', None)
+        password = request.form.get('password', None)
+        phone = request.form.get('phone', None)
+        address = request.form.get('address', None)
 
-    if register != None :
+    if register != None:
+
+        user = User.query.filter(User.username == username).first()
+        if user != None:
+            flash('نام کاربری دیگری انتخاب کنید.')
+            return redirect(url_for('user.login'))
+
+
         user = User(username=username, password=sha256_crypt.encrypt(password), phone=phone, address=address)
         db.session.add(user)
         db.session.commit()
-        login_user(users)
+        login_user(user)
 
         return redirect('/user/dashboard')
+
+    else:
+        user = User.query.filter(User.username == username).first()
+        if user == None:
+            flash('نام کاربری یا رمز اشتباه است.')
+            return redirect(url_for('user.login'))
+
+        if sha256_crypt.verify(password, user.password):
+            login_user(user)
+            return redirect('/user/dashboard')
+        else:
+            flash('نام کاربری یا رمز اشتباه است.')
+            return redirect(url_for('user.login'))

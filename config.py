9683b5b35a377e9076a777f265916706500e6228
iliya-varm <iliyavarnaserim@gmail.com>
2024-03-21 23:48:09 +0330

    order added to user dashoard

--- config.py
+++ config.py
@@ -8,4 +8,5 @@ SECRET_KEY = 'asdfghjklpoiuytrewqzxcvbnmzvxbnmadsfdgfhjkwtyui'
 
 PAYMENT_MERCHANT= 'sandbox'
 PAYMENT_CALLBACK= 'http://localhost:5000/verify'
+PAYMENT_FIRST_REQUEST_URL= 'https://sandbox.shepa.com/api/v1/token'
 PAYMENT_VARIFY_REQUEST_URL= 'https://sandbox.shepa.com/api/v1/verify'

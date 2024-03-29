1e9a95ed648cb9e5a0f754f6d0b2acc74c6cd3b0
iliya-varm <iliyavarnaserim@gmail.com>
2024-03-30 01:19:31 +0330

    edit info and logout added to user dashboard

--- blueprints/user.py
+++ blueprints/user.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
-from flask_login import login_user, login_required, current_user
+from flask_login import login_user, login_required, current_user, logout_user
 from passlib.hash import sha256_crypt
 import config
 from extentions import db
@@ -157,14 +157,44 @@ def verify():
     return redirect(url_for('user.dashboard'))
 
 
-@app.route('/user/dashboard', methods=['GET'])
+@app.route('/user/dashboard', methods=['GET', 'POST'])
 @login_required
 def dashboard():
-    return render_template('user/dashboard.html')
+    if request.method == 'GET':
+        return render_template('user/dashboard.html')
+    else:
+        username = request.form.get('username', None)
+        password = request.form.get('password', None)
+        phone = request.form.get('phone', None)
+        address = request.form.get('address', None)
+
+        if current_user.username == username:
+            user = User.query.filter(User.username == username).first()
+            if user != None:
+                flash('نام کاربری دیگری انتخاب کنید.')
+                return redirect(url_for('user.dashboard'))
+        else:
+            current_user.username = username
+
+        if password != None :
+            current_user.password = sha256_crypt.encrypt(password)
+        current_user.address = address
+        current_user.phone = phone
+
+        db.session.commit()
+        flash('تغیرات با موفقیت ثبت شد')
+        return redirect(url_for('user.dashboard'))
+
+
+@app.route('/user/logout', methods=['GET'])
+@login_required
+def logout():
+    logout_user()
+    return redirect('/')
 
 
 @app.route('/user/dashboard/order/<id>', methods=['GET'])
 @login_required
 def order(id):
-    cart = current_user.carts.filter(Cart.id==id).first_or_404()
+    cart = current_user.carts.filter(Cart.id == id).first_or_404()
     return render_template('user/order.html', cart=cart)

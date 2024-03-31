5c726916d715255f5814b84c09cc7375efed889d
iliya-varm <iliyavarnaserim@gmail.com>
2024-03-31 06:03:45 +0330

    slide show for main page

--- blueprints/general.py
+++ blueprints/general.py
@@ -3,15 +3,17 @@ from sqlalchemy.sql.expression import func
 
 from models.product import Product
 
-app = Blueprint('general', __name__)
+app = Blueprint("general", __name__)
 
 
 @app.route('/')
 def main():
     search = request.args.get('search', None)
+
     products = Product.query.filter(Product.active == 1)
+
     if search != None:
-        products = products.filter(Product.name.like(f'%{search}%')).all()
+        products = products.filter(Product.name.like(f'%{search}%'))
 
     products = products.order_by(func.random()).all()
 
@@ -23,12 +25,12 @@ def product(id, name):
     product = Product.query.filter(Product.id == id).filter(Product.name == name).filter(
         Product.active == 1).first_or_404()
 
-    another_product = Product.query.filter(Product.active == 1).filter(
+    another_products = Product.query.filter(Product.active == 1).filter(
         Product.name.like(f'%{product.name[0:5]}%')).order_by(func.random()).limit(3).all()
 
-    return render_template('product.html', product=product, another_product=another_product)
+    return render_template('product.html', product=product, another_products=another_products)
 
 
 @app.route('/about')
-def about():  # put application's code here
+def about():
     return render_template('about.html')
